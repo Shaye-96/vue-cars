df3d30cf38942889e750d39aa27a595d9c01f746
Shaye-96 <1151032202@qq.com>
2020-12-26 15:41:26 +0800

    shaye 汽车详情

--- web/src/components/carsItem/index.vue
+++ web/src/components/carsItem/index.vue
@@ -25,7 +25,8 @@
               <li></li>
               <li></li>
             </ul>
-            <span>约 <span class="kilo">600</span> KM</span>
+
+            <span class="distance">约 <span class="kilo">600</span> KM</span>
           </div>
         </div>
       </div>
@@ -33,13 +34,39 @@
         <a class="paking" href="">某某某停车场</a>
       </footer>
     </section>
+    <div class="cars-item cars-details" :style="'height:' + height">
+      <h4 class="paking"><a class="paking" href="">某某某停车场</a></h4>
+      <header>
+        <h4 class="cars-logo">
+          <img class="logo" src="../../assets/images/car_logo.jpg" alt="" />
+        </h4>
+        <span class="name">Mustang 2019款</span>
+        <span class="details">新能源汽车 5座</span>
+      </header>
+      <img class="cars" src="../../assets/images/car.png" alt="" />
+      <div class="cars-desc">
+        <span class="number">粤B745N8</span>
+        <span class="distance">约 <span class="kilo">600</span> KM</span>
+      </div>
+      <div class="ele-wrap">
+        <div class="ele">
+        </div>
+      </div>
+      
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: "carsItem",
+  props: {
+    height: {
+      type: [String, Number],
+      default: "257px"
+    }
+  }
+};
 </script>
 
 <style lang="scss">
